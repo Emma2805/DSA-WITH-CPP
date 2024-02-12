f7a610b6e0d69c31c2b5110a56f95115d79a30b7
Sweksha Raj <94289820+Emma2805@users.noreply.github.com>
2024-02-12 09:08:11 +0530

    Add files via upload

---
+++ reference Variable/useOfRefVariable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int update(int &n){
+    n++;
+}
+int main(){
+    int n =5;
+    cout<<"before "<<n<<endl;
+    update(n);
+    cout<<"after "<<n<<endl;
+}
