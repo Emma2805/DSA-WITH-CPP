66ffaedafc257613d27f5cc3de525f7f64c4cbef
Sweksha Raj <94289820+Emma2805@users.noreply.github.com>
2024-02-16 03:04:41 +0530

    Add files via upload

---
+++ Dynamic Memory Allocation(heap)/dynamicArray.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int getsum(int *arr, int n){
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    //variable size array
+    int *arr = new int[n];
+
+    //taking input in array
+    for(int i =0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    cout<<"sum "<<getsum(arr, n);
+
+}
